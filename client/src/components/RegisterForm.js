e0365737813ea5d14b73d75e4398529a66b24661
Ahmed <syedahmedali.0520@gmail.com>
2025-07-01 23:38:18 -0400

    assignment 4

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,42 +1,49 @@
-const Register = () => {
+import React from 'react';
 
-  return(
-    <div>
-      <form >
+const RegisterForm = () => {
+  return (
+    <div className="card shadow-sm p-4">
+      <h4 className="text-center mb-4">Create an Account</h4>
+      <form>
         <div className="mb-3">
-          <label htmlFor="username" className="form-label">Username</label>
-          <input 
-            type="text" 
-            className="form-control" 
-            id="username"
-            name='username'
+          <label htmlFor="registerUsername" className="form-label">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            id="registerUsername"
+            name="username"
             required
           />
         </div>
+
         <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password</label>
-          <input 
-            type="password" 
-            className="form-control" 
-            id="password"
-            name='password'
+          <label htmlFor="registerPassword" className="form-label">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="registerPassword"
+            name="password"
             required
           />
         </div>
-        <div className="mb-3">
-          <label htmlFor="password2" className="form-label">Confirm Password</label>
-          <input 
-            type="password" 
-            className="form-control" 
-            id="password2"
-            name='password2'
+
+        <div className="mb-4">
+          <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="confirmPassword"
+            name="password2"
             required
           />
         </div>
-        <input type="submit" className="btn btn-primary" value="Register"/>
+
+        <div className="d-grid">
+          <button type="submit" className="btn btn-outline-primary fw-bold">Register</button>
+        </div>
       </form>
     </div>
   );
-}
+};
 
-export default Register;
+export default RegisterForm;
