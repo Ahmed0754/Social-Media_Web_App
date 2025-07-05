a90019f82afa0bef7d8d702ee9be09305ac04e03
Ahmed <syedahmedali.0520@gmail.com>
2025-07-05 01:26:15 -0400

    assignment 4

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,33 +1,42 @@
-const Login = () => {
+import React from 'react';
 
-  return(
-    <div>
-      <form >
-        <div className="mb-3">
-          <label htmlFor="username" className="form-label">Username</label>
-          <input 
-            type="text" 
-            className="form-control" 
-            id="username"
-            name='username'
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password</label>
-          <input 
-            type="password" 
-            className="form-control" 
-            id="password"
-            name='password'
-            required
-          />
+const LoginForm = () => {
+  return (
+    <div className="card shadow-sm p-4 mb-5">
+      <h4 className="text-center mb-4">Login to Your Account</h4>
+      <form>
+        <div className="row mb-3">
+          <label htmlFor="loginUsername" className="col-sm-4 col-form-label">Username</label>
+          <div className="col-sm-8">
+            <input
+              type="text"
+              className="form-control"
+              id="loginUsername"
+              name="username"
+              required
+            />
+          </div>
         </div>
 
-        <input type="submit" className="btn btn-primary" value="Login"/>
+        <div className="row mb-3">
+          <label htmlFor="loginPassword" className="col-sm-4 col-form-label">Password</label>
+          <div className="col-sm-8">
+            <input
+              type="password"
+              className="form-control"
+              id="loginPassword"
+              name="password"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">Login</button>
+        </div>
       </form>
     </div>
   );
-}
+};
 
-export default Login;
+export default LoginForm;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,35 +2,38 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Social Media App</a>
-        <button
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarNav" 
-          aria-controls="navbarNav" 
-          aria-expanded="false" 
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="#">Profile</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Register</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Login</a>
-            </li>
-          </ul>
+    <header className="shadow-sm">
+      <nav className="navbar navbar-expand-lg bg-primary navbar-dark px-4 py-2 rounded-bottom">
+        <div className="container-fluid">
+          <a className="navbar-brand fw-bold" href="#">Social Media App</a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#mainNavbar"
+            aria-controls="mainNavbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse justify-content-end" id="mainNavbar">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">Profile</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Register</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Login</a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 };
 
